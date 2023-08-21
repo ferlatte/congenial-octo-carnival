1f24dfb87024ec4a961a94d38916519b3e0b4642
Mark Ferlatte <mark@truss.works>
2023-08-21 13:05:36 -0700

    Hello world

---
+++ congenial-octo-carnival/congenial-octo-carnival/congenial_octo_carnivalApp.swift
@@ -0,0 +1,17 @@
+//
+//  congenial_octo_carnivalApp.swift
+//  congenial-octo-carnival
+//
+//  Created by Mark Ferlatte on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct congenial_octo_carnivalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
